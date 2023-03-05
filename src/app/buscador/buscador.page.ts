cd25aecc5857a0d913d268bc9f4d0cd391a272dc
Joaquin Nuñez <nunezjoac@gmail.com>
2023-03-05 19:12:05 -0300

    Subo cambios al buscador page

--- src/app/buscador/buscador.page.ts
+++ src/app/buscador/buscador.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-buscador',
@@ -8,53 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class BuscadorPage implements OnInit {
 
   public list = [
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
-    {
-      nombre : "Nahse"
-    },
   ]
+
+  public listMostar = []
+  private tamanoList = 0
+
+  public seachUrl = ""
+  public inputPlaceHolder = ""
+  public input = true
+
+  @ViewChild("list", { read: ElementRef }) private listComponent: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.listMostar = this.list
+    this.tamanoList = this.listMostar.length
   }
 
 
   filtrar(event) {
     const filtro = event.detail.value.toLowerCase();
-    //this.novedadesAMostrar = this.novedades.filter(novedad => novedad.nombre.toLowerCase().includes(filtro));
+    this.listMostar = this.list.filter(item => item.nombre.toLowerCase().includes(filtro));
+    if(this.tamanoList != this.listMostar.length){
+      this.tamanoList = this.listMostar.length
+      this.animList(this.listComponent.nativeElement)
+    }
   }
+
+  animList(item) {
+    item.classList.remove('animacion');
+    void item.offsetWidth;
+    item.classList.add('animacion');
+  }
+
+  clickItem(item){
+    item.srcElement.classList.add('animacion-click');
+  }
+
 
 }
